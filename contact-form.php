f072d686cae972e663ad7ac37eba005d599b52ff
fdortega <66751959+fdortega@users.noreply.github.com>
2020-09-08 12:37:09 -0500

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,9 +1,9 @@
 <?php
 
-if (isset($_POST['Send Message'])) {
-  $name = $_POST['name'];
-  $mailfrom = $_POST['email'];
-  $message = $_POST['message'];
+if (isset($_POST["submit"])) {
+  $name = $_POST["name"];
+  $mailfrom = $_POST["email"];
+  $message = $_POST["message"];
 
   $mailTo = "felix.ortega@live.com";
   $headers = "From: ".$mailfrom;
