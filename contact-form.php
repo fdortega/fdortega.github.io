267d3299ddda4bf9b4a7f08a26d6faa6a154ae98
fdortega <66751959+fdortega@users.noreply.github.com>
2020-09-08 12:44:33 -0500

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -10,7 +10,6 @@ if (isset($_POST["submit"])) {
   $txt = "You have received an email from ".$name.".\n\n".$message;
 
   mail($mailTo, $txt, $header);
-  header("Location: index.php?mailsend");
 }
 
 ?>
